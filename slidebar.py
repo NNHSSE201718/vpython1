10acd7b5b6d504e60de9bc229deef00da28d78ca
Adam Pi <adampatni27@gmail.com>
2018-04-04 15:10:25 -0500

    slidebar controls work, still fails at binding

--- slidebar.py
+++ slidebar.py
@@ -1,4 +1,13 @@
 from vpython import *
-ball=sphere(pos=vector(4,7,3),radius=2,color=color.green)
-
-print("a")
+print(vpython.__version__)
+def change(): # Called by controls when button clicked
+    if b.text == 'Click me':
+        b.text = 'Try again'
+    else:
+        b.text = 'Click me'
+ 
+c = controls() # Create controls window
+print(controls)
+# Create a button in the controls window:
+b = button( pos=(10,10), width=60, height=60, 
+              text='Click me', action=lambda: change() )
