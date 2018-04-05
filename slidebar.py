3d15441b4b249a84bfed820540b823bc9d3da1ba
karenge1 <kge@stu.naperville203.org>
2018-04-05 15:10:49 -0500

    found a demo that seems to fix bind problem

--- slidebar.py
+++ slidebar.py
@@ -1,13 +1,18 @@
 from vpython import *
+#from vpython.controls import * doesn't work
 print(vpython.__version__)
+
 def change(): # Called by controls when button clicked
     if b.text == 'Click me':
         b.text = 'Try again'
     else:
         b.text = 'Click me'
  
-c = controls() # Create controls window
-print(controls)
+#c = controls() # Create controls window
+#print(controls)
 # Create a button in the controls window:
 b = button( pos=(10,10), width=60, height=60, 
               text='Click me', action=lambda: change() )
+
+#c = controls(title='Controlling the Scene',x=0, y=400, width=300, height=300, range=50)
+
