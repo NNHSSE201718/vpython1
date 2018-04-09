b90915faf4a2954f76f480cbcb7110f8553c5fa5
karenge1 <kge@stu.naperville203.org>
2018-04-09 15:10:58 -0500

    works with python 2

--- slidebar.py
+++ slidebar.py
@@ -1,6 +1,7 @@
-from vpython import *
+from visual import *
+from visual.controls import *
 #from vpython.controls import * doesn't work
-print(vpython.__version__)
+print(visual.__version__)
 
 def change(): # Called by controls when button clicked
     if b.text == 'Click me':
@@ -8,11 +9,11 @@ def change(): # Called by controls when button clicked
     else:
         b.text = 'Click me'
  
-#c = controls() # Create controls window
-#print(controls)
+c = controls() # Create controls window
+print(controls)
 # Create a button in the controls window:
 b = button( pos=(10,10), width=60, height=60, 
               text='Click me', action=lambda: change() )
 
-#c = controls(title='Controlling the Scene',x=0, y=400, width=300, height=300, range=50)
-
+c = controls(title='Controlling the Scene',x=0, y=400, width=300, height=300,
+             range=50)
