5a68ce730fee5eb3192237c69cfc901b8a5185d1
Adam Patni <adampatni27@gmail.com>
2018-04-12 15:13:58 -0500

    implemented the sliders with graph, need to figure out how to clear graph

--- slidebar.py
+++ slidebar.py
@@ -2,14 +2,21 @@ from vpython import *
 from graph_functions import graph_simpletrig, graph_sumtrig
 print(vpython.__version__)
 
-global all_graphs = [[0,0,1,0],[0,0,1,0],[0,0,1,0]]
-print(all_graphs)
+amt_of_bars = 5
+all_graphs = []
+for i in range(amt_of_bars):
+    all_graphs.append([0,0,1,0])
+
 def setAmplitude(s):
-    
+    global all_graphs
+    global allBars
+    all_graphs[allBars.index(s)][1]=s.value
+    print(all_graphs)
+    createGraph()
+def createGraph():
+    global all_graphs
+    graph_sumtrig(all_graphs,color.blue)
 
-
-def createGraph(graph_list):
-    graph_sumtrig(graph_list,color.blue)
-
-
-bar1 = slider(min=0, max=5, value=3, length=100, bind=createGraph(all_graphs), right=90)
+allBars = []
+for i in range(amt_of_bars):
+    allBars.append(slider(min=0, max=5, value=3, length=100, bind=setAmplitude, right=90))
