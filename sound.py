f8682a1251c553ea3f7935624326cf5b2e4065eb
Adam Patni <adampatni27@gmail.com>
2018-05-15 06:04:18 -0500

    Removed excess files from master

--- sound.py
+++
@@ -1,4 +0,0 @@
-import winsound
-duration = 10000  # millisecond
-freq = 440  # Hz
-winsound.Beep(freq, duration)
--- vpython_demo.py
+++
@@ -1,7 +0,0 @@
-from vpython import *
-import math
-
-sphere()
-
-f1 = gcurve(color=color.blue, graph=gd)
-f1.plot(pos=(x,cos(x)))
